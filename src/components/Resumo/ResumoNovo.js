659625cf9498e36290c4fb40704417ccecd400ef
davi38 <davi4alves@gmail.com>
2022-07-21 16:43:56 -0300

    pdf link

--- src/components/Resumo/ResumoNovo.js
+++ src/components/Resumo/ResumoNovo.js
@@ -7,8 +7,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumoLinkAutoAtualiza =
-  "https://raw.githubusercontent.com/davi38/portfolio/master/src/Assets/DaviAlves-webdeveloper.pdf";
+const resumoLinkAutoAtualiza = "https://raw.githubusercontent.com/davi38/portfolio/master/src/Assets/DaviAlves-webdeveloper.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
