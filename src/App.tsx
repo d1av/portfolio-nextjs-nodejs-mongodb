930bdc4988326ed624e404cd6184c435e25b6032
davi38 <davi4alves@gmail.com>
2022-09-13 22:49:50 -0300

    feat: project structure complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from "react";
 import "./Globals.scss";
+
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+import { Navbar } from "./components";
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
     </div>
   );
 }
