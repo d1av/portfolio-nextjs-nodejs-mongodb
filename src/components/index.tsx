9dc2f7a11822add3cc7a8e3ac6a204d3a2ff2e7e
davi38 <davi4alves@gmail.com>
2022-09-13 23:35:09 -0300

    feat: navbar and wrapper

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,1 +1,5 @@
-export {default as Navbar} from './Navbar/Navbar'
+import NavigationDots from "./NavigationDots";
+import SocialMedia from "./SocialMedia";
+import Navbar from "./Navbar/Navbar";
+
+export { NavigationDots, SocialMedia, Navbar };
