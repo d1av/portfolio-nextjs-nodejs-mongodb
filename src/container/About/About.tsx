03e576ffd3981ec7d4d903422b886867881eddea
davi38 <davi4alves@gmail.com>
2022-09-24 17:39:21 -0300

    feat: about style

--- src/container/About/About.tsx
+++ src/container/About/About.tsx
@@ -27,22 +27,21 @@ const About = () => {
   return (
     <>
       <h2 className="head-text">
-        I know that
-        <span> Good Design</span>
-        <br />
-        means
-        <span> Good Bussiness</span>
+        I Know that <span>Good Design</span> <br />
+        means <span>Good Business</span>
       </h2>
+
       <div className="app__profiles">
         {abouts.map((about, index) => (
           <motion.div
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
             transition={{ duration: 0.5, type: "tween" }}
-            className="app__profiles"
+            className="app__profile-item"
+            key={about.title + index}
           >
             <img src={about.imgUrl} alt={about.title} />
-            <h2 className="bold_text" style={{ marginTop: 20 }}>
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
             <p className="p-text" style={{ marginTop: 10 }}>
