0f839de01688bfef6d3b0084bdc645e31d66b373
davi38 <davi4alves@gmail.com>
2022-09-24 19:38:48 -0300

    feat: Lateral Navigation

--- src/wrapper/AppWrap.tsx
+++ src/wrapper/AppWrap.tsx
@@ -9,10 +9,10 @@ const AppWrap = (Component:any, idName?:any, classNames?:any) =>
         <div className="app__wrapper app__flex">
           <Component />
 
-          <div className="copyright">
-            <p className="p-text">@2020 MICHAEL</p>
+          {/* <div className="copyright">
+            <p className="p-text">@2022 Davi</p>
             <p className="p-text">All rights reserved</p>
-          </div>
+          </div> */}
         </div>
         <NavigationDots active={idName} />
       </div>
