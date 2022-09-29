5043e6e9200a92098331124476eed87eb58371a5
davi38 <davi4alves@gmail.com>
2022-09-29 16:31:19 -0300

    fix: image and header

--- src/constants/images.tsx
+++ src/constants/images.tsx
@@ -33,7 +33,14 @@ import nb from "../assets/nb.png";
 import skype from "../assets/skype.png";
 import spotify from "../assets/spotify.png";
 
+import mongodb from "../assets/mongodb.png";
+import nodejs from "../assets/nodejs.png";
+import postgresql from "../assets/postgresql.png";
+
 export default {
+  postgresql,
+  nodejs,
+  mongodb,
   Davi_Logo,
   email,
   mobile,
